05affcc01a7399d55f45c27568909bc6e584299b
Daniel R Gorgonha <rogergorgonha@gmail.com>
2022-11-22 20:09:55 -0300

    feat: added character handling

--- hangman.py
+++ hangman.py
@@ -5,8 +5,24 @@ def play():
   print("***Welcome to the hangman game!***")
   print("**********************************")
 
-  print("End of the game")
+  palavra_secreta = "banana"
 
+  enforcou = False
+  acertou = False
+
+  while(not enforcou and not acertou):
+
+    chute = input("Qual letra? ")
+    
+    index = 0
+    for letra in palavra_secreta:
+      if(chute == letra):
+        print("Encontrei a letra {} na possição {}".format(letra, index))
+      index = index + 1
+    
+    print("jogando ...")
+
+  print("End of the game")
 
 if(__name__ == "__main__"):
   play()
